3aec18df8f5cf52b396099f8ba5b1f43f7b2455d
Igor Stalmach <92231661+igorstalmach@users.noreply.github.com>
2023-04-04 11:46:51 +0200

    clang fixes

--- so.cpp
+++ so.cpp
@@ -6,74 +6,74 @@
 #include <mutex>
 #include <pthread.h>
 
-std::queue<int> tasktable;
-std::queue<int> beertable;
-pthread_mutex_t myMutex;
-pthread_mutex_t beer_mutex;
-pthread_mutex_t task_mutex;
-pthread_cond_t	beer_is_empty = PTHREAD_COND_INITIALIZER;
-pthread_cond_t	task_is_empty = PTHREAD_COND_INITIALIZER;
-pthread_cond_t	beer_is_full = PTHREAD_COND_INITIALIZER;
-pthread_cond_t	task_is_full = PTHREAD_COND_INITIALIZER;
+std::queue<int> taskQueue;
+std::queue<int> beerQueue;
+pthread_mutex_t workerMutex;
+pthread_mutex_t beerMutex;
+pthread_mutex_t taskMutex;
+pthread_cond_t	beerIsEmpty = PTHREAD_COND_INITIALIZER;
+pthread_cond_t	taskIsEmpty = PTHREAD_COND_INITIALIZER;
+pthread_cond_t	beerIsFull = PTHREAD_COND_INITIALIZER;
+pthread_cond_t	taskIsFull = PTHREAD_COND_INITIALIZER;
 
 void project_manager(int id) {
     while(true){
-        pthread_mutex_lock(&task_mutex);
-        if(tasktable.size() < 100){
+        pthread_mutex_lock(&taskMutex);
+        if(taskQueue.size() < 100){
             std::this_thread::sleep_for(std::chrono::milliseconds(rand() % 100 + 1));
             std::cout<<"dodano task\n";
-            tasktable.push(rand() % 10 + 1);
-            pthread_cond_signal(&task_is_empty);
+            taskQueue.push(rand() % 10 + 1);
+            pthread_cond_signal(&taskIsEmpty);
         }
         else{
             std::cout<<"czekam na wykonanie taska: "<<id<<std::endl;
-            pthread_cond_wait(&task_is_full, &task_mutex);
+            pthread_cond_wait(&taskIsFull, &taskMutex);
         }
-        pthread_mutex_unlock(&task_mutex);
+        pthread_mutex_unlock(&taskMutex);
     }
 }
 
 void beer_manager(int id) {
     while(true){
-        pthread_mutex_lock(&beer_mutex);
-        if(beertable.size() < 100){
+        pthread_mutex_lock(&beerMutex);
+        if(beerQueue.size() < 100){
             std::this_thread::sleep_for(std::chrono::milliseconds(rand() % 100 + 1));
             std::cout<<"dodano piwo\n";
-            beertable.push(rand() % 10 + 1);
-            pthread_cond_signal(&beer_is_empty);
+            beerQueue.push(rand() % 10 + 1);
+            pthread_cond_signal(&beerIsEmpty);
         }
         else{
             std::cout<<"czekam na wypicie piwa: "<<id<<std::endl;
-            pthread_cond_wait(&beer_is_full, &beer_mutex);
+            pthread_cond_wait(&beerIsFull, &beerMutex);
         }
-        pthread_mutex_unlock(&beer_mutex);
+        pthread_mutex_unlock(&beerMutex);
     }
 
 }
 
 void worker(int id) {
     while(true){
-        pthread_mutex_lock(&myMutex);
-        if (beertable.size() > 0) {
-            if(tasktable.size() > 0){
-                std::this_thread::sleep_for(std::chrono::milliseconds(1*beertable.front()*tasktable.front()));
+        pthread_mutex_lock(&workerMutex);
+        if (!beerQueue.empty()) {
+            if(!taskQueue.empty()){
+                std::this_thread::sleep_for(std::chrono::milliseconds(1 * beerQueue.front() * taskQueue.front()));
                 std::cout<<"wykonano task\n";
-                beertable.pop();
-                tasktable.pop();
-                pthread_cond_signal(&task_is_full);
-                pthread_cond_signal(&beer_is_full);
+                beerQueue.pop();
+                taskQueue.pop();
+                pthread_cond_signal(&taskIsFull);
+                pthread_cond_signal(&beerIsFull);
             }
             else{
                 std::cout<<"czekam na taski: "<<id<<std::endl;
-                pthread_cond_wait(&task_is_empty, &myMutex);
+                pthread_cond_wait(&taskIsEmpty, &workerMutex);
             }
           
         }
         else{
             std::cout<<"czekam na piwo: "<<id<<std::endl;
-			pthread_cond_wait(&beer_is_empty, &myMutex);
+			pthread_cond_wait(&beerIsEmpty, &workerMutex);
         }
-        pthread_mutex_unlock(&myMutex);
+        pthread_mutex_unlock(&workerMutex);
     }
 }
 
@@ -83,15 +83,15 @@ int main(int argc, char **argv) {
     std::vector<std::jthread> beer_managers;
 
     for(int i = 0; i<std::stoi(std::string(argv[1])); i++){
-        workers.push_back(std::jthread(worker, i));
+        workers.emplace_back(worker, i);
     }
 
     for(int i = 0; i<std::stoi(std::string(argv[2])); i++){
-        project_managers.push_back(std::jthread(project_manager, i));
+        project_managers.emplace_back(project_manager, i);
     }
 
     for(int i = 0; i<std::stoi(std::string(argv[3])); i++){
-        beer_managers.push_back(std::jthread(beer_manager, i));
+        beer_managers.emplace_back(beer_manager, i);
     }
 
     for(int i = 0; i<std::stoi(std::string(argv[1])); i++){
